44322c3f0e5cc05579b9fc0c4b31a90bebab8fa6
LeandroAugusto89 <102551622+LeandroAugusto89@users.noreply.github.com>
2023-07-16 23:07:45 -0300

    Conexão com o banco de dados e demais configurações

---
+++ SistemaCadastro/Controllers/ContatoController.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SistemaCadastro.Models;
+using SistemaCadastro.Repositorio;
+
+namespace SistemaCadastro.Controllers
+{
+    
+    public class ContatoController : Controller
+    {
+        private readonly IContatoRepositorio _contatoRepositorio;
+
+        public ContatoController(IContatoRepositorio contatoRepositorio)
+        {
+            _contatoRepositorio = contatoRepositorio;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Criar()
+        {
+            return View();
+        }
+
+        public IActionResult Editar()
+        {
+            return View();
+        }
+
+        public IActionResult ApagarConfirmacao()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Criar(ContatoModel contato)
+        {
+            _contatoRepositorio.Adicionar(contato);
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ SistemaCadastro/Data/BDContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SistemaCadastro.Models;
+
+namespace SistemaCadastro.Data
+{
+    public class BDContext : DbContext
+    {
+        public BDContext (DbContextOptions <BDContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<ContatoModel> Contato { get; set; }
+    }
+}
